f4fb78d0f7cc6663ec80b28316f880eabf069887
techguru8237 <techguru8237@gmail.com>
2025-02-10 08:54:01 +0900

    Fix env import error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_BACKEND_URL: string;
+  // Add other environment variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
