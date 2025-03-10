a65743ec53d398e1735b96fc64f66f9de88b6975
techguru8237 <techguru8237@gmail.com>
2025-03-10 17:13:27 +0900

    Fix create table issue

--- src/hooks/useUploadForm.ts
+++ src/hooks/useUploadForm.ts
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
+import { User } from "../types";
 
 export const useUploadForm = (url: string) => {
   const [isSuccess, setIsSuccess] = useState(false);
   const [progress, setProgress] = useState(0);
 
+  const currentUserString = localStorage.getItem("currentUser");
+  const currentUser: User = currentUserString ? JSON.parse(currentUserString) : null; // Parse the string into a User object
+
   const uploadForm = async (formData: FormData) => {
     await axios.post(url, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
-        authorization: `Bearer ${localStorage.getItem("token")}`,
+        authorization: `Bearer ${currentUser.token}`,
       },
       onUploadProgress: (progressEvent) => {
         if (progressEvent.total) {
